bcfaab229c1fc0bf905bcf00ec52ba2ec2e69fa5
Matthew Sherborne <msherborne@gmail.com>
2021-02-13 23:53:39 +1000

    Removed redundant complexity
    The purgecss is not done in postcss anymore, but as part of tailwind
    itself, so there's no point in having it in 2 places

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
-        process.env.NODE_ENV === 'production' ? require('cssnano')({ preset: 'default' }) : null,
-        process.env.NODE_ENV === 'production' ? require('@fullhuman/postcss-purgecss')({
-            content: ["./source_code/src/**/*.rs", "./source_code/index.html"],
-            defaultExtrator: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-        }) : null
-    ]
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 }
