f94fa9a44255196f809d33b7ce6494f9b0a11a40
Matthew Sherborne <matt.sherborne@rackspace.com>
2021-02-20 09:41:24 +1000

    Made tailwind watch for changes in tailwind.css
    Because I had a .btn in there, and in the source I only had
    `class="btn"` but in tailwind.css I had all the sub-attributes:
    
        .btn {
           @apply rounded py-2 px-3 shadow uppercase text-xs font-bold cursor-pointer tracking-wider;
        }
    
    .. and it was stripping bits I needed(like tracking-wider) from the
    production css, because it was nowhere else in the source.

--- input/tailwind.config.js
+++ input/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     enabled: process.env.NODE_ENV === 'production',
     mode: 'all',
     // source_code represents the rust (yew?) source code root
-    content: ["./source_code/src/**/*.rs", "./source_code/index.html"]
+    content: ["./source_code/src/**/*.rs", "./source_code/index.html", "./input/tailwind.css"]
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
