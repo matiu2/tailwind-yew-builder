2226ffc70dc09517779c5903e7c745eea6ee08e7
Matthew Sherborne <matt.sherborne@rackspace.com>
2021-02-20 09:43:44 +1000

    Added status comments to prod.sh
    So you can see what's happening.

--- prod.sh
+++ prod.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-npx tailwindcss-cli@latest build /work/input/tailwind.css --minify --purge -c /work/input/tailwind.config.js -o /work/output/tailwind.big.css && \
-cleancss /work/output/tailwind.big.css -o /work/output/tailwind.css && \
+echo "Building tailwind.big.css"
+npx tailwindcss-cli@latest build /work/input/tailwind.css --minify --purge -c /work/input/tailwind.config.js -o /work/output/tailwind.big.css
+
+echo "Removing old output/tailwind.css"
+rm /work/output/tailwind.css
+
+echo "Shrinking..."
+cleancss /work/output/tailwind.big.css -o /work/output/tailwind.css
+
+echo "Removing output/tailwind.big.css"
 rm /work/output/tailwind.big.css
+
