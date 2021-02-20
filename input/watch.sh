39b96ed7576dd3cc169450c8515ba5c0dda380c3
Matthew Sherborne <matt.sherborne@rackspace.com>
2021-02-20 10:09:59 +1000

    Made watch.sh refresh and print the file list every time

--- input/watch.sh
+++ input/watch.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-files="input/tailwind.css input/tailwind.config.js $(find source_code/src -type d)  $(find source_code/src -type f -name '*.rs') source_code/index.html"
-echo waiting for changes in these files: $files
-while IFS= inotifywait $files
+
+wait_for_changes() {
+    FILES="input/tailwind.css input/tailwind.config.js $(find source_code/src -type d)  $(find source_code/src -type f -name '*.rs') source_code/index.html"
+    echo
+    echo Waiting for changes in these files: $FILES
+    inotifywait $FILES
+}
+
+while :
 do
+    wait_for_changes
     cd /work && /bin/sh prod.sh
 done
